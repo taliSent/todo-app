548de3f66ef857a21981b6aeebab3359c5f758ae
nsentemova <sentemovanatalya@yandex.ru>
2023-02-05 17:03:23 +0300

    eslint added

--- src/components/Header/components/BackgroundImage/BackgroundImage.tsx
+++ src/components/Header/components/BackgroundImage/BackgroundImage.tsx
@@ -6,13 +6,11 @@ import "@/scss/components/_background-image.scss";
 const BackgroundImage: FC = () => {
   const { isDarkTheme } = useContext(Context);
   return (
-    <>
-      <img
-        src={getBackgroundSrc(isDarkTheme)}
-        alt='background'
-        className='background-image'
-      />
-    </>
+    <img
+      src={getBackgroundSrc(isDarkTheme)}
+      alt='background'
+      className='background-image'
+    />
   );
 };
 export default BackgroundImage;
