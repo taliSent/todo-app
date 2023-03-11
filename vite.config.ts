4e5cc24aa27e6e7c9df329e8f02768023636cff5
nsentemova <sentemovanatalya@yandex.ru>
2023-03-11 18:53:43 +0300

    deploy fix?

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression({ deleteOriginFile: true })],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
