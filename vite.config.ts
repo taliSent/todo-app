f28688ab553c0741104b7f3937444b5afd376447
nsentemova <sentemovanatalya@yandex.ru>
2023-03-11 17:06:24 +0300

    compression fix?

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()],
+  plugins: [react(), viteCompression({ deleteOriginFile: true })],
   resolve: {
     alias: {
       "@": "/src",
